a0c0de605e93a3d389eeecc59080ef61c3bc1066
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-07-04 18:19:37 +0800

    commit

--- select_subreddits.py
+++ select_subreddits.py
@@ -12,6 +12,6 @@ args = parser.parse_args()
 ss = SparkSession.builder.getOrCreate()
 cmnt_df = None
 for f in args.rc:
-    df = ss.read.json(f).select('subreddit_id')
+    df = ss.read.json(f).select('subreddit', 'subreddit_id')
     cmnt_df = df if cmnt_df is None else cmnt_df.union(df)
 cmnt_df.groupBy('subreddit_id').count().toPandas().sort_values(by='count', ascending=False).reset_index().to_hdf('count', '/df')
