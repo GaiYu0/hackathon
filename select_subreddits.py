a102061ef140d9010f953c82115064c1cccd7de1
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-07-04 17:12:09 +0800

    commit

---
+++ select_subreddits.py
@@ -0,0 +1,18 @@
+import argparse
+
+from pyspark.sql.functions import regexp_replace
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import IntegerType
+
+import utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rc', type=str, nargs='+')
+
+args = parser.parse_args()
+
+cmnt_df = None
+for f in args.rc:
+    df = ss.read.json(f).select()
+    cmnt_df = df if cmnt_df is None else cmnt_df.union(df)
+cmnt_df.groupBy('subreddit_id').count().toPandas().sort_values(by='count', ascending=False).reset_index().to_hdf('count', '/df')
