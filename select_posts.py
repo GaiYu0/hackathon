ad18983f59a2f5d5f678e81e901303f8a120ed51
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-07-05 13:16:13 +0800

    commit

--- select_posts.py
+++ select_posts.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 ss = SparkSession.builder.getOrCreate()
 post_df = None
 for f in args.rs:
-    df = ss.read.json(f).select('author', 'id', 'subreddit', 'subreddit_id', 'title')
+    df = ss.read.json(f).select('id', 'subreddit', 'subreddit_id', 'title')
     post_df = df if post_df is None else post_df.union(df)
 subreddit_ids = pickle.load(open('subreddit_ids', 'rb'))
 post_df.filter(post_df.subreddit_id.isin(*subreddit_ids)).coalesce(1).write.json('RS', mode='overwrite')
