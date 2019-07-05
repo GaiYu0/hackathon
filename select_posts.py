9cd5b4c713b4284379056ad83ed17deb273968ce
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-07-05 11:30:35 +0800

    commit

--- select_posts.py
+++ select_posts.py
@@ -14,4 +14,4 @@ for f in args.rs:
     df = ss.read.json(f).select('author', 'id', 'subreddit', 'subreddit_id', 'title')
     post_df = df if post_df is None else post_df.union(df)
 subreddit_ids = pickle.load(open('subreddit_ids', 'rb'))
-post_df.filter(post_df.subreddit_id.isin(*subreddit_ids)).to_json('RS')
+post_df.filter(post_df.subreddit_id.isin(*subreddit_ids)).write.json('RS')
